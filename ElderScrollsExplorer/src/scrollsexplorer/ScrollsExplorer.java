23555800c11925173cea691eed172754da03a4db
phil <phil@Thinky>
2015-09-19 01:35:15 +0200

    output primitive version number system to logs

--- ElderScrollsExplorer/src/scrollsexplorer/ScrollsExplorer.java
+++ ElderScrollsExplorer/src/scrollsexplorer/ScrollsExplorer.java
@@ -52,6 +52,7 @@ import bsa.BSAFileSet;
 import bsa.source.BsaMeshSource;
 import bsa.source.BsaSoundSource;
 import bsa.source.BsaTextureSource;
+import client.BootStrap;
 
 import com.gg.slider.SideBar;
 import com.gg.slider.SideBar.SideBarMode;
@@ -505,14 +506,16 @@ public class ScrollsExplorer extends JFrame implements BethRenderSettings.Update
 		//Arguments for goodness
 		//-Xmx1200m -Xms900m -Dj3d.cacheAutoComputeBounds=true -Dsun.java2d.noddraw=true 
 		//-Dj3d.sharedctx=true -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC
-		
+
 		//Can't see a noticable diff in perf with this one
 		//-Dj3d.soleUser=true 				If set to true, allow the sole-user optimization; otherwise, sole-user is disabled 
-		
+
 		// still can't tell if this improves things
 		// it'ss only help if interleave is on
 		//-Dj3d.optimizedForSpace=false		If set to true, optimize by-ref geometry for space; otherwise, optimize for rendering speed 
-		
+
+		System.out.println("VERSION: " + BootStrap.CURRENT_ZIP_VERSION);
+		System.err.println("VERSION: " + BootStrap.CURRENT_ZIP_VERSION);
 
 		//jogl recomends for non phones 
 		System.setProperty("jogl.disable.opengles", "true");
