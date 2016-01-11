4b9ee424143f0d19a59b7d53a5869de8debdd8df
phil <phil@Thinky>
2016-01-12 00:28:08 +0100

    Sky made NIO buffer

--- ElderScrollsExplorer/src/scrollsexplorer/simpleclient/scenegraph/SimpleSky.java
+++ ElderScrollsExplorer/src/scrollsexplorer/simpleclient/scenegraph/SimpleSky.java
@@ -3,9 +3,14 @@ package scrollsexplorer.simpleclient.scenegraph;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Background;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.GeometryArray;
 import javax.media.j3d.Group;
+import javax.media.j3d.J3DBuffer;
+import javax.media.j3d.QuadArray;
 import javax.media.j3d.RenderingAttributes;
 import javax.media.j3d.Texture;
+import javax.media.j3d.TriangleStripArray;
 
 import scrollsexplorer.GameConfig;
 import tools3d.utils.Utils3D;
@@ -63,10 +68,29 @@ public class SimpleSky extends BranchGroup
 		// to enable the texture image to be rendered
 		// and because we are *inside* the Sphere we have to generate 
 		// Normal coordinates inwards or the Sphere will not be visible.
-		Sphere sphere = new Sphere(1.0f, Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS_INWARD, app);
+		Sphere sphere = new Sphere(1.0f, Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD, app);
 
 		// start wiring everything together,
 
+		//Make in NIO
+		TriangleStripArray geo = (TriangleStripArray) sphere.getShape().getGeometry();
+		int[] stripVertexCounts = new int[geo.getNumStrips()];
+		geo.getStripVertexCounts(stripVertexCounts);
+		TriangleStripArray newGeo = new TriangleStripArray(geo.getVertexCount(), GeometryArray.COORDINATES | GeometryArray.NORMALS
+				| GeometryArray.TEXTURE_COORDINATE_2 | GeometryArray.USE_NIO_BUFFER | GeometryArray.BY_REFERENCE, stripVertexCounts);
+
+		float[] coords = new float[geo.getValidVertexCount() * 3];
+		geo.getCoordinates(0, coords);
+		newGeo.setCoordRefBuffer(new J3DBuffer(Utils3D.makeFloatBuffer(coords)));
+		float[] norms = new float[geo.getValidVertexCount() * 3];
+		geo.getNormals(0, norms);
+		newGeo.setNormalRefBuffer(new J3DBuffer(Utils3D.makeFloatBuffer(norms)));
+		float[] textcoords = new float[geo.getValidVertexCount() * 2];
+		geo.getTextureCoordinates(0, 0, textcoords);
+		newGeo.setTexCoordRefBuffer(0, new J3DBuffer(Utils3D.makeFloatBuffer(textcoords)));
+
+		sphere.getShape().setGeometry(newGeo);
+		
 		// add the Sphere to its parent BranchGroup.
 		bgGeometry.addChild(sphere);
 
@@ -81,7 +105,7 @@ public class SimpleSky extends BranchGroup
 		/*Background background = new Background();
 		background.setApplicationBounds(Utils3D.defaultBounds);
 		//background.setColor(new Color3f(1.0f, 1.0f, 1.0f));
-
+		
 		BranchGroup bgNifbg = new BranchGroup();
 		NifFile nifFile = NifToJ3d.loadNiObjects("meshes\\sky\\stars.nif", meshSource);
 		NiToJ3dData niToJ3dData = new NiToJ3dData(nifFile.blocks);
@@ -94,9 +118,9 @@ public class SimpleSky extends BranchGroup
 				//bgNifbg.addChild(jnts);
 			}
 		}
-
+		
 		background.setGeometry(bgNifbg);
-
+		
 		BranchGroup bgbg = new BranchGroup();
 		bgbg.addChild(background);*/
 
